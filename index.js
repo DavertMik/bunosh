797d9cdccda5baa885b30adc989bf3b4faa430eb
DavertMik <davert@testomat.io>
2025-09-02 02:30:52 +0300

    added ai tasks

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ import exec from "./src/tasks/exec.js";
 import fetch from "./src/tasks/fetch.js";
 import writeToFile from "./src/tasks/writeToFile.js";
 import copyFile from "./src/tasks/copyFile.js";
+import ai from "./src/tasks/ai.js";
 import { ask, yell, say } from "./src/io.js";
 import { task, stopOnFail, ignoreFail } from "./src/task.js";
 
-export { exec, fetch, writeToFile, copyFile, ask, yell, say, task, stopOnFail, ignoreFail };
+export { exec, fetch, writeToFile, copyFile, ai, ask, yell, say, task, stopOnFail, ignoreFail };
 
 
 export function buildCmd(cmd) {
@@ -20,6 +21,7 @@ global.bunosh = {
   exec,
   writeToFile,
   copyFile,
+  ai,
   stopOnFail,
   ignoreFail,
   task,
